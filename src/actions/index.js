b0d5f4c7a6cd5d4e7050d15a8509c8feb607e43f
ericfoster <eric_foster73@hotmail.com>
2017-11-16 23:42:56 -0500

    install axios for ajax requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,11 @@
 const API_KEY = '23ecdf925600f2cc665b9bfd0b7f9efc';
+const ROOT_URL = `http://api.openweather.org/data/2.5/forceast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  // hardcoded the country code to United States
+  const url = `${ROOT_URL}&q=${city},us`;
   return {
     type: FETCH_WEATHER
   };
