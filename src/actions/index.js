7739eee02ebf15fe738a00c72fa19b9b8851e441
ericfoster <eric_foster73@hotmail.com>
2017-11-16 23:21:02 -0500

    add heroku build pack

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 const API_KEY = '23ecdf925600f2cc665b9bfd0b7f9efc';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+  return {
+    type: FETCH_WEATHER
+  };
+}
