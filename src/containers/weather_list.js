6a2de5904efcea0c0770cffc250efb1f5905108e
ericfoster <eric_foster73@hotmail.com>
2017-11-18 14:40:12 -0500

    map over the cities to the state and render them.  Also install sparklines

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,25 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+/*
+TODO:
+  Investigate issue:
+  If an incorrect city name is passed in the searchbar,
+  then the API will return a 404 and the the state
+  will fail to update anymore.
+*/
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.name;
+    /* The json response from the api is not the same as
+    the course.  Instead oc cityData.city.name, it must be
+    cityData.name */
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
   render() {
     return (
       <table className="table table-hover">
@@ -13,7 +31,12 @@ class WeatherList extends Component {
             <th>Humidity</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {this
+            .props
+            .weather
+            .map(this.renderWeather)}
+        </tbody>
       </table>
     );
   }
