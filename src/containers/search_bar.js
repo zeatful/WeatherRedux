0c7e3f6d38e78ec9efed7f179d3dd99762141c97
ericfoster <eric_foster73@hotmail.com>
2017-11-25 00:08:38 -0500

    just guessing at this point...

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,7 +5,7 @@ import {fetchWeather} from '../actions/index';
 
 class SearchBar extends Component {
   constructor(props) {
-    super(props);
+    Component.super(props);
 
     this.state = {
       term: ''
