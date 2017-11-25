631cb0fcff27ffc23fbca5a067581215460a72b4
ericfoster <eric_foster73@hotmail.com>
2017-11-25 00:26:19 -0500

    testing

--- src/components/chart.js
+++ src/components/chart.js
@@ -6,16 +6,14 @@ function average(data) {
   return _.round(_.sum(data) / data.length);
 }
 
-export default class Chart extends Component {
-  render() {
-    return (
-      <div>
-        <Sparklines height={120} width={180} data={props.data}>
-          <SparklinesLine color={props.color}/>
-          <SparklinesReferenceLine type="avg"/>
-        </Sparklines>
-        <div>{average(props.data)} {props.units}</div>
-      </div>
-    );
-  }
+export default(props) => {
+  return (
+    <div>
+      <Sparklines height={120} width={180} data={props.data}>
+        <SparklinesLine color={props.color}/>
+        <SparklinesReferenceLine type="avg"/>
+      </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
+    </div>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ['./src/index.js'],
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    umdNamedDefine: true
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
