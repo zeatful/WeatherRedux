f4a73390d82c0cec6beb2b84fa51cf62a7c75365
ericfoster <eric_foster73@hotmail.com>
2017-11-25 00:31:19 -0500

    try to fix issue with import

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import Chart from '../components/chart';
+import * as Chart from '../components/chart';
 import _ from 'lodash';
 import GoogleMap from '../components/google_map';
 
